bcf64189875a894623b82eb85351b137de84dd63
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-08-21 21:08:58 -0500

    Agregando script del reto 2

---
+++ reto2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+regexedad='^([1-9]{1,2})$'
+regexnombre='^[[:alpha:]]*$'
+regexapellido='^[[:alpha:]]*$'
+regexdireccion='^\s*\w'
+regexnumero='^[0-9]{10})$'
+
+read -p "Ingrese su nombre: " nombre
+read -p "Ingrese su Apellido: " apellido
+read -p "Ingrese su edad: " edad
+read -p "ingrese su dirección: " direccion
+read -p "ingrese su numero de telefono: " telefono
+
+if [[ $edad =~ $regexedad ]]; then
+	echo "edad valida"
+else
+	echo "edad invalida"
+fi
+
+if [[ $nombre =~ $regexnombre ]]; then
+	echo "nombre valido"
+else
+	echo "nombre invalido"
+fi
+
+if [[ $apellido =~ $regexapellido ]]; then
+	echo "apellido valido"
+else
+	echo "apellido invalido"
+fi
+
+if [[ $direccion =~ $regexdireccion ]]; then
+	echo "Dirección valida"
+else
+	echo "Dirección invalida"
+fi
+
+if [[ $telefono =~ $regextelefono ]]; then
+	echo "telefono valido"
+else
+	echo "telefono invalido"
+fi
+
+echo "Los datos registrados son: "
+echo $nombre
+echo $apellido
+echo $edad
+echo $direccion
+echo $telefono
