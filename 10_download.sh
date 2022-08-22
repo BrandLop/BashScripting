6d60d27293c357d70f219b10236fc3110bbb4514
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-08-21 21:08:36 -0500

    Agregando script que descarga un archivo con wget

---
+++ 10_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+
+echo "Descargar información de internet"
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.23/bin/apache-tomcat-10.0.23.zip 
